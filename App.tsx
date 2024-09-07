3879bb86c4ef4b608dbd557068a3b1fc915c3cb8
huudungg <huudung2004kt@gmail.com>
2024-09-07 19:49:40 +0700

    TouchableWithoutFeedback and keyboard dismiss

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
-
-import { useState } from 'react';
-import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
-import { Header } from '@rneui/themed';
+import React, { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 interface Todo {
   id: number,
@@ -17,69 +15,57 @@ export default function App() {
     { id: 4, name: 'e' },
     { id: 5, name: 'r' },
     { id: 6, name: '6' },
-  ])
+  ]);
 
-  const [todo, setTodo] = useState<string>()
+  const [todo, setTodo] = useState<string>("");
 
   const addTodo = () => {
-    alert("clicked " + todo)
-    const newTodo: Todo = { id: generateUDI(), name: todo };
-    setListTodo([newTodo, ...listTodo])
-    setTodo('')
-  }
+    if (todo) {
+      const newTodo: Todo = { id: listTodo.length + 1, name: todo };
+      setListTodo([newTodo, ...listTodo]);
+      setTodo(''); // Reset input field
+    } else {
+      alert("Todo cannot be empty!");
+    }
+  };
 
   const deleteTodo = (id: number) => {
-    alert("delete " + id)
-    setListTodo(listTodo.filter(item => (item.id !== id)))
-  }
+    alert("Delete Todo ID: " + id);
+    setListTodo(listTodo.filter(item => item.id !== id));
+  };
 
   const generateUDI = (): number => {
-    // Lấy thời gian hiện tại tính bằng mili giây (số lớn, để tránh trùng lặp)
     const timestamp = Date.now();
-
-    // Tạo một số ngẫu nhiên
-    const randomNum = Math.floor(Math.random() * 1000000); // 1 số ngẫu nhiên từ 0 đến 999999
-
-    // Kết hợp timestamp và số ngẫu nhiên để tạo ra một số độc nhất
+    const randomNum = Math.floor(Math.random() * 1000000);
     const udid = parseInt(`${timestamp}${randomNum}`);
-
     return udid;
-  }
+  };
 
   return (
-    <>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.container}>
         <Text style={styles.header}>Tran Huu Dung</Text>
-        <View>
-          <TextInput onChange={
-            (text) => {
-              setTodo(text.nativeEvent.text);
-            }
-          } value={todo} style={styles.inputField} />
-        </View>
+        <TextInput
+          onChangeText={(text) => setTodo(text)}
+          value={todo}
+          style={styles.inputField}
+          placeholder="Enter a todo"
+        />
         <View style={styles.btn}>
-          <Button onPress={() => {
-            addTodo()
-          }} title='Add' />
+          <Button onPress={addTodo} title='Add' />
         </View>
-        <View>
-          <FlatList
-            data={listTodo}
-            renderItem={
-              ({ item }) => (
-                <>
-                  <Text>id: {item.id} | name: {item.name} </Text>
-                  <Button onPress={() => {
-                    deleteTodo(item.id)
-                  }} title='delete'></Button>
-                </>
-              )
-            }
-            keyExtractor={item => item.id as unknown as string}
-          />
-        </View>
+        <FlatList
+          data={listTodo}
+          renderItem={({ item }) => (
+            <View style={styles.todoItem}>
+              <Text>id: {item.id} | name: {item.name} </Text>
+              <Button onPress={() => deleteTodo(item.id)} title='Delete' />
+            </View>
+          )}
+          keyExtractor={item => item.id.toString()}
+        />
       </View>
-    </>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -94,22 +80,24 @@ const styles = StyleSheet.create({
     backgroundColor: 'blue',
     justifyContent: 'center',
     color: '#fff',
-    fontSize: 55,
+    fontSize: 30,
     textAlign: 'center',
     paddingHorizontal: 33,
-    borderBlockColor: '1px black'
   },
   inputField: {
     width: 300,
     height: 50,
-    borderBlockColor: 'black',
+    borderColor: 'black',
     borderWidth: 1,
-    marginTop: 30
-  }, btn: {
-    backgroundColor: 'red',
+    marginTop: 30,
+    paddingHorizontal: 10,
+  },
+  btn: {
     width: 100,
     marginTop: 20,
-    color: 'black'
-  }
-
+  },
+  todoItem: {
+    marginVertical: 10,
+    alignItems: 'center',
+  },
 });
